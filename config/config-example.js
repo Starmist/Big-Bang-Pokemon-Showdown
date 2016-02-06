6d702795009b084550b98bc28c0b9338db2cd0d8
LinkedSoul <LinkedSoul@users.noreply.github.com>
2016-02-06 10:49:54 -0600

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -10,6 +10,9 @@ exports.port = 8000;
 //   know what you are doing.
 exports.proxyip = ['127.0.0.0/8']; 
 
+exports.serverid = 'victory';
+exports.servertoken = 'cxFDIl405M0o';
+
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
 //   in every Random Battle team.
