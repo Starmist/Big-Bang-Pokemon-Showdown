8fee3c79ce538507c212a1561341280406d47f86
Josué Ludenberg Oberlin Universe <bjrc1412@hotmail.com>
2016-02-09 12:24:36 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,7 +9,8 @@ exports.port = 8000;
 exports.proxyip = ['127.0.0.0/8'];
 
 exports.serverid = 'victory';
-exports.servertoken = 'Nnhml6PJlxDn';
+
+exports.servertoken = 'xijgEKdZnuf5';
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
